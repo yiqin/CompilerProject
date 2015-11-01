44b3c53bc3c133af390a0c892f204415f0b9fdc4
Matthew Emery Goss <egoss@stanley.cs.uchicago.edu>
2015-11-01 15:24:08 -0600

    Replace catch source with single library header. Untrack test/testCatch binary.

--- test/testCatch.cpp
+++ test/testCatch.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch/catch.hpp"
+#include "catch.hpp"
 
 TEST_CASE( "Test Cases for Compiler", "[factorial]" ) {
 
